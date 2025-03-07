acf59937a65eefe6d7b926250e2b80bb94f7acef
Sindre Grindheim <sindre.grindheim@pm.me>
2025-03-07 16:31:30 +0100

    Added performance improvements for google imports in bigquery module

--- dbtwiz/bigquery.py
+++ dbtwiz/bigquery.py
@@ -1,12 +1,31 @@
+from importlib import import_module
 from typing import List, Tuple
 
-from google.api_core.exceptions import Forbidden, NotFound
-from google.cloud import bigquery
 from ruamel.yaml.scalarstring import PreservedScalarString
+
+
+def import_module_once(parent, subpackage, submodule=None):
+    """Function to enable lazy import and only import once."""
+    full_module_name = f"{parent}.{subpackage}" if parent else subpackage
+    if full_module_name not in globals():
+        globals()[full_module_name] = import_module(full_module_name)
+    # If submodule is specified, get it from the imported module
+    if submodule:
+        return getattr(globals()[full_module_name], submodule)
+    return globals()[full_module_name]
+
+
+def import_common_modules():
+    """Import function for common packages."""
+    bigquery = import_module_once("google.cloud", "bigquery")
+    Forbidden = import_module_once("google.api_core", "exceptions", "Forbidden")
+    NotFound = import_module_once("google.api_core", "exceptions", "NotFound")
+    return bigquery, Forbidden, NotFound
 
 
 def list_datasets_in_project(project) -> Tuple[List[str], str]:
     """Fetch all datasets in the given project from BigQuery."""
+    bigquery = import_module_once("google.cloud", "bigquery")
     client = bigquery.Client()
     try:
         datasets = list(client.list_datasets(project=project))
@@ -17,6 +36,7 @@ def list_datasets_in_project(project) -> Tuple[List[str], str]:
 
 def fetch_tables_in_dataset(project, dataset) -> Tuple[List[str], str]:
     """Fetch all tables in the given project and dataset from BigQuery."""
+    bigquery, Forbidden, NotFound = import_common_modules()
     client = bigquery.Client()
     dataset_ref = f"{project}.{dataset}"
     try:
@@ -32,6 +52,7 @@ def fetch_tables_in_dataset(project, dataset) -> Tuple[List[str], str]:
 
 def fetch_table_columns(project, dataset, table_name) -> Tuple[List[str], str]:
     """Fetch column names and descriptions from BigQuery."""
+    bigquery, Forbidden, NotFound = import_common_modules()
     client = bigquery.Client()
     table_ref = f"{project}.{dataset}.{table_name}"
     try:
@@ -53,6 +74,7 @@ def fetch_table_columns(project, dataset, table_name) -> Tuple[List[str], str]:
 
 def check_project_exists(project) -> str:
     """Checks whether the given project exists in BigQuery"""
+    bigquery, Forbidden, NotFound = import_common_modules()
     client = bigquery.Client()
     try:
         # Check if the project exists and is accessible
@@ -68,12 +90,16 @@ def check_project_exists(project) -> str:
     except Exception as e:
         return f"Error: Failed to verify project '{project}': {e}"
 
+
 def run_bq_query(project, query):
     """Runs a query in bigquery"""
+    bigquery = import_module_once("google.cloud", "bigquery")
     client = bigquery.Client(project=project)
     return client.query(query)
 
+
 def delete_bq_table(table_id):
     """Deletes a bq table"""
+    bigquery = import_module_once("google.cloud", "bigquery")
     client = bigquery.Client()
     client.delete_table(table_id)
