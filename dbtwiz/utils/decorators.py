9a8c15bdacc770fdbd7ed07f313d113a328ea915
Sindre Grindheim <sindre.grindheim@pm.me>
2025-04-07 10:35:33 +0200

    Added decorator for extended command documentation

--- dbtwiz/utils/decorators.py
+++ dbtwiz/utils/decorators.py
@@ -8,3 +8,15 @@ def examples(*example_commands):
         return func
 
     return decorator
+
+
+def description(*description_commands):
+    """Custom decorator to add extended description for typer commands."""
+
+    def decorator(func):
+        if not hasattr(func, "_command_description"):
+            func._command_description = []
+        func._command_description.extend(description_commands)
+        return func
+
+    return decorator
