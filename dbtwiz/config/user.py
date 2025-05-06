219b7729696b1b85b4c6ddbaa7c7176375bf07a8
Lars Haugseth <git@larshaugseth.com>
2025-05-06 11:30:49 +0200

    New and simplified UserConfig class.

--- dbtwiz/config/user.py
+++ dbtwiz/config/user.py
@@ -1,11 +1,10 @@
-import configparser
 import functools
 from pathlib import Path
+import tomllib
 from typing import Any, Dict
-
 import typer
 
-from dbtwiz.helpers.logger import error, info
+from dbtwiz.helpers.logger import error, fatal, info
 
 
 @functools.cache
@@ -16,144 +15,77 @@ def user_config():
 
 def user_config_path(target: str = "") -> Path:
     """Get Path to the given target within the user configuration directory"""
-    return user_config().CONFIG_PATH / target
-
-
-def dark_mode() -> bool:
-    """Are we using a dark theme?"""
-    return user_config().get("general", "theme") == "dark"
-
-
-class InvalidConfig(ValueError):
-    """Class for invalid config."""
-
-    pass
+    return user_config().config_path() / target
 
 
 class UserConfig:
-    """User-specific configuration"""
+    """User-specific settings from config.toml"""
 
-    CONFIG_PATH = Path(typer.get_app_dir("dbtwiz"))
-    CONFIG_FILE = CONFIG_PATH / "config.ini"
-
-    THEMES: Dict[str, Any] = dict(
-        names=["light", "dark"],
-        colors=dict(
-            name=[30, 115],
-            path=[27, 147],
-            tags=[28, 106],
-            group=[94, 178],
-            materialized=[54, 212],
-            owner=[136, 208],
-            policy=[136, 208],
-            dep_stg=[34, 118],
-            dep_int=[24, 123],
-            dep_mart=[20, 75],
-            description=[102, 144],
-            deprecated=[124, 196],
-        ),
-    )
-
-    SECTIONS = [
-        {
-            "name": "general",
-            "description": "General settings",
-            "settings": [
-                ("auth_check", bool, True, ""),
-                ("editor", str, "code", "Command used to open a file for editing"),
-                ("theme", str, "light", "Colour theme for model information"),
-            ],
-        },
-        {
-            "name": "model_info",
-            "description": "Settings relating to model information output",
-            "settings": [
-                ("formatter", str, "fmt -s", "Command used to format model information text"),
-            ],
-        },
-        {
-            "name": "theme",
-            "description": "ANSI colour codes for highlighting",
-            "settings": [
-                ("name", int, 30, "Model name"),
-                ("path", int, 27, "Model file path"),
-                ("tags", int, 28, "Model tag list"),
-                ("group", int, 94, "Model group list"),
-                ("materialized", int, 54, "Model materialization type"),
-                ("owner", int, 136, "Model owner"),
-                ("policy", int, 136, "Model policies"),
-                ("dep_stg", int, 34, "Model dependencies (staging)"),
-                ("dep_int", int, 24, "Model dependencies (intermediate)"),
-                ("dep_mart", int, 20, "Model dependencies (marts)"),
-                ("description", int, 102, "Model description"),
-                ("deprecated", int, 124, "Model description for deprecated models"),
-            ]
-        },
-    ]
+    DEFAULTS = {
+        "auth_check": True,
+        "editor": "code",
+        "model_formatter": "fmt -s",
+        "theme": "light",
+    }
 
 
-    def __init__(self):
-        """Get the configuration, from file if it exists or create a new one with defaults"""
-        self.parser = configparser.ConfigParser()
-        if Path.exists(self.CONFIG_FILE):
-            self.parser.read(self.CONFIG_FILE)
-            return
-        self._set_defaults()
+    def __init__(self) -> None:
+        """Initialize the class by determining the root path and parsing the config."""
+        self._parse_config()
 
-    def get(self, section, key) -> str:
-        """Retrieve a string value from the specified section and key."""
-        return self.parser.get(section, key)
 
-    def getint(self, section, key) -> int:
-        """Retrieve an integer value from the specified section and key."""
-        return self.parser.getint(section, key)
+    def config_path(self) -> Path:
+        """Path to user configuration directory"""
+        return Path(typer.get_app_dir("dbtwiz"))
 
-    def getboolean(self, section, key) -> int:
-        """Retrieve a boolean value from the specified section and key."""
-        return self.parser.getboolean(section, key)
 
-    def color(self, key) -> int:
-        """Get the color value of a named key in the current theme"""
-        return self.parser.getint("theme", key)
+    def _config_file(self) -> Path:
+        """Path to user configuration directory"""
+        return self.config_path() / "config.toml"
 
-    def update(self, section, key, value) -> None:
-        """Update a setting and write changes to configuration file"""
+
+    def _parse_config(self):
+        """Parse the config file"""
+        config_file = self._config_file()
+        if not config_file.exists():
+            self._make_default_config()
         try:
-            if not self.parser.has_section(section):
-                raise InvalidConfig(f"Unknown configuration section: {section}")
-            if not self.parser.has_option(section, key):
-                raise InvalidConfig(f"Unknown configuration setting: {section}:{key}")
+            with open(self._config_file(), "rb") as f:
+                self._config = tomllib.load(f)
+        except Exception as ex:
+            fatal(f"Failed to parse file {self._config_file()}: {ex}")
 
-            if section == "general" and key == "theme":
-                if value not in self.THEMES["names"]:
-                    raise InvalidConfig(
-                        f"Invalid theme: {value} - must be one of {self.THEMES['names']}"
-                    )
-                self._set_theme(theme=value)
+
+    def _make_default_config(self):
+        """Generate default configuration"""
+        defaults = ""
+        for key, value in UserConfig.DEFAULTS.items():
+            defaults += f"{key} = "
+            if isinstance(value, str):
+                defaults += '"' + value + '"\n'
+            elif isinstance(value, bool):
+                defaults += str(value).lower() + "\n"
             else:
-                self.parser.set(section, key, value)
-            self._write_to_file()
-            info(f"Configuration setting {section}:{key} updated.")
-        except InvalidConfig as ex:
-            error(str(ex))
+                defaults += str(value) + "\n"
+        with open(self._config_file(), "w") as f:
+            f.write(defaults)
 
-    def _write_to_file(self) -> None:
-        """Write the current parser configuration to a file."""
-        Path.mkdir(self.CONFIG_PATH, exist_ok=True)
-        with open(self.CONFIG_FILE, "w+") as f:
-            self.parser.write(f)
 
-    def _set_defaults(self) -> None:
-        """Initialize the parser with default settings and sections."""
-        for section in self.SECTIONS:
-            self.parser.add_section(section)
-        self.parser.set("general", "auth_check", "yes")
-        self.parser.set("model_info", "formatter", "fmt -s")
-        self._set_theme(self.THEMES["names"][0])
+    def __getattr__(self, name):
+        """Dynamically handle attribute access and warn if the setting is missing."""
+        if name in self._config:
+            value = self._config[name]
+            if not value or value == "":
+                fatal(
+                    f"'{name}' config is undefined in tool.dbtwiz.project config in pyproject.toml"
+                )
+            return value
+        else:
+            fatal(
+                f"'{name}' is missing from tool.dbtwiz.project config in pyproject.toml"
+            )
+            return None  # or raise AttributeError if you prefer
 
-    def _set_theme(self, theme) -> None:
-        """Apply the specified theme to the parser configuration."""
-        self.parser.set("general", "theme", theme)
-        theme_index = self.THEMES["names"].index(theme)
-        for key, value in self.THEMES["colors"].items():
-            self.parser.set("theme", key, str(value[theme_index]))
+    def __dir__(self):
+        """Include dynamic attributes for autocompletion."""
+        return list(self._config.keys()) + list(super().__dir__())
